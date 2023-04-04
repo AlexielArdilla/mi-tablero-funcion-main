729b28bc31f29195d9342c564bc4024ef3ed1fe0
Alejandro G. Vera <eldiosvolador@hotmail.com>
2023-04-03 23:04:11 -0300

    pasé de clases a functions el código

--- src/tablero_components/Equipo.jsx
+++ src/tablero_components/Equipo.jsx
@@ -1,16 +1,15 @@
-import { Component } from "react";
 
-class Equipo extends Component {
-  render() {
-    return (
-      <div className="tablero_cabecera">
-        <h3>{ this.props.title }</h3>
-        <h3>{ this.props.contador }</h3>
+function Equipo(props) {
 
-        <img className='img_sel' src={ this.props.bandera } alt="" />
+
+  return (
+    <div className="tablero_cabecera">
+        <h3>{props.title}</h3>
+        <h3>{props.contador}</h3>
+
+        <img className='img_sel' src={props.bandera} alt="" />
       </div>
-    )
-  }
+  );
 }
 
 export default Equipo;
